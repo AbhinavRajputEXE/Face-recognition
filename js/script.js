53ddfbcfb885df38ff1cda077efb6c6a533e605f
Abhinav Rajput <80591258+AbhinavRajputEXE@users.noreply.github.com>
2022-05-17 13:41:22 +0530

    Face recognition v2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+/* Declarations */
 const video = document.getElementById('video')
+
+/* Promises */
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -6,6 +9,8 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
+
+/* Functiom to srart and show video from the webcam */
 
 function startVideo() {
   navigator.getUserMedia(
@@ -15,10 +20,12 @@ function startVideo() {
   )
 }
 
+/* Async/await function to detect face expressions, landmarks and draw canvas around the face and show the values accordnigly */
+
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.getElementById("face").append(canvas)
-  const displaySize = { width: video.width, height: 560 }
+  const displaySize = { width: 720, height: 560 }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
@@ -28,6 +35,26 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
+
+/* Async/await function to detect face expressions and show text in text box accordingly */
+
+setInterval(async()=>{
+    const detect = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+    let sad = detect[0].expressions.sad
+    let angry = detect[0].expressions.angry
+    let surprised = detect[0].expressions.surprised
+    let happy = detect[0].expressions.happy
+    /* Jokes Array */
+    let jokeArray = [
+      "Why do we tell actors to break a leg?....Because every play has a cast.",
+      "Did you hear about the claustrophobic astronaut?.....He just needed a little space.",
+      "Why don't scientists trust atoms?....Because they make up everything.",
+      "Why did the chicken go to the séance?....To get to the other side."
+  ];
+    if(sad>=0.7) document.getElementById("joke").innerHTML = jokeArray[Math.floor(Math.random() * jokeArray.length)];
+    else if(happy>=0.8) document.getElementById("joke").innerHTML = "Are you happy? 😃 You should be SAD lol!";
+    else document.getElementById("joke").innerHTML = "You look fine today!";
+},5000)
 
 /* DARK MODE CODE */
 const chk = document.getElementById('chk');
